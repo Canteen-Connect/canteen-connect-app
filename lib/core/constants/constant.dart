5a0e73bcae3eb62f323d27073d2ff195683ad16f
saalim199 <salimahmad199@gmail.com>
2024-07-21 19:53:55 +0530

    abc

---
+++ lib/core/constants/constant.dart
@@ -0,0 +1,5 @@
+const MONGO_URL =
+    "mongodb+srv://salimahmad199:8808726160@cluster0.3car9qo.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+
+// Run the canteen-connect-backend and update this with your own ipaddress
+const URL = 'http://localhost:3000/api/v1';
